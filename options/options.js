dc3a06fe43a58790840f1379c8ab3e1bbfd8652e
Crowdbotics <team@crowdbotics.com>
2023-03-01 04:34:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wippy-39169",
+  url: "https://wippy-39169.botics.co",
+  api: "https://wippy-39169.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
